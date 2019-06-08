b2cdc8ed4e38e166af9a4acac50cdfdf0a198c4b
sarpong <abasimisarpong@rocketmail.com>
2019-06-08 23:16:55 +0100

    refactored app to use express routers

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require('express')
 ,app= express();
 
 
-app.get('/', (req,res)=>{
-  res.send('Hello world')
-})
+app.use('/', require('./routes/index'))
 
 
 const PORT = process.env.PORT || 3000
