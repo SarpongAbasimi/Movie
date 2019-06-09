be5ed7e85e3eaa27262d724cf5314c0560a0ae65
sarpong <abasimisarpong@rocketmail.com>
2019-06-09 12:44:07 +0100

    implemented the function to fetch to make api call from movie db api

--- controllers/api/movies.js
+++ controllers/api/movies.js
@@ -1,3 +1,15 @@
+const fetch = require('node-fetch')
+
 exports.movies = (req,res)=> {
-  res.send('Hello worlds');
-}
+  const baseurl= process.env.BASEURL,
+  apikey = process.env.MOVIEDB_API_KEY
+  fetch(`${baseurl}${apikey}`)
+  .then(response =>{
+    if(response.ok){
+      return response.json()
+    }
+    throw new err('Request failed')
+  }).then(jsonResponse => {
+    res.send(jsonResponse)
+  })
+};
