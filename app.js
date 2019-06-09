32e6a60cbfdca2c7c39fb93d6e7888aa24915c08
sarpong <abasimisarpong@rocketmail.com>
2019-06-09 10:29:51 +0100

    changed the path for the / path from the index route to api/movies

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const express = require('express')
 ,app= express();
 
-
-app.use('/', require('./routes/index'))
-
+app.use('/', require('./routes/api/movies'))
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, ()=>{
   console.log(`listening on port ${PORT}`)
 })
-
 
 module.exports = app
