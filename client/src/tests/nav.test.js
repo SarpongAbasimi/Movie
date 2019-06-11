1b117ad3277b73a12f76010c98a5f55a0e151ece
sarpong <abasimisarpong@rocketmail.com>
2019-06-11 21:55:56 +0100

    Fixed test error on navBar component

--- client/src/tests/nav.test.js
+++ client/src/tests/nav.test.js
@@ -5,9 +5,31 @@ import { shallow } from 'enzyme';
 import { Nav } from '../components/nav';
 Enzyme.configure({ adapter: new Adapter() });
 
-// describe('<Nav/>', ()=> {
-//   it('should render hello on the home page', ()=> {
-//     const wrapper = shallow(<Nav/>)
-//     expect(wrapper.find('div')).toHaveLength(5)
-//   });
-// });
+describe('<Nav/>', ()=> {
+  it('It Has a length of one', ()=> {
+    const navLinks = [
+        {id: 1, type: "DISCOVER"},
+        {id: 2, type: "MOVIES"},
+        {id: 3, type: "TV SHOWS"},
+        {id: 4, type: "LOGIN"},
+        {id: 5, type: "SIGN UP"},
+      ],
+     wrapper = shallow(<Nav link={ navLinks }/>)
+     expect(wrapper.find('.navbar')).toHaveLength(1)
+  });
+
+  it('contains name to link inside of it', ()=>{
+    const navLinks = [
+        {id: 1, type: "DISCOVER"},
+        {id: 2, type: "MOVIES"},
+        {id: 3, type: "TV SHOWS"},
+        {id: 4, type: "LOGIN"},
+        {id: 5, type: "SIGN UP"},
+      ],
+     wrapper = shallow(<Nav link={ navLinks }/>)
+
+     const links = wrapper.find('.navBarLink').map(node => node.text());
+     expect(links).toEqual(['DISCOVER', 'MOVIES', 'TV SHOWS', 'LOGIN', 'SIGN UP']);
+  })
+});
+
