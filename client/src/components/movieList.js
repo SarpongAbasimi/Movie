7d0d0715c8323421dc0dc034c5a13a530fb37749
sarpong <abasimisarpong@rocketmail.com>
2019-06-12 09:35:01 +0100

    started movie nav component

---
+++ client/src/components/movieList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export class MovieList extends React.Component{
+  render(){
+    return(
+      <div>
+      {console.log(this.props)}
+        Hey
+      </div>
+    );
+  }
+}
