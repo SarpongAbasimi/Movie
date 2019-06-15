3a37d291c3cd76eb6751dea15b7984b0dfb15fa4
sarpong <abasimisarpong@rocketmail.com>
2019-06-15 12:35:39 +0100

    created routes for movies.js

--- routes/api/movies.js
+++ routes/api/movies.js
@@ -2,6 +2,7 @@ const express = require('express')
 ,router = express.Router()
 ,movies = require('../../controllers/api/movies');
 
-router.get('/',movies.movies)
+router.get('/', movies.movies)
+router.get('/popular/movies', movies.popularMovies)
 
 module.exports = router
