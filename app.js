5d81f4dd0052fb1bdd25ee3153ee9da1e743455d
sarpong <abasimisarpong@rocketmail.com>
2019-06-16 13:53:37 +0100

    wrote test for 404 page and added a 404 template to views directory

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
+,path = require('path')
 ,app= express();
 
 require('dotenv').config()
-app.use('/api/movies', require('./routes/api/movies'))
-app.use('*', require('./routes/errorpages/404'))
+app.set('view engine', 'ejs');
+app.use('/api/movies', require('./routes/api/movies'));
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use('*', require('./routes/errorpages/404'));
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, ()=>{
--- controllers/errors/404.js
+++ controllers/errors/404.js
@@ -1,3 +1,3 @@
 exports.error404 = (req, res, next)=>{
-  res.status(404).send('Sorry Mate the page can\'t be found');
+  res.status(404).render('404page');
 }
