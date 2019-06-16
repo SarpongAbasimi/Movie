60cc1f51f7f5464776ae90fe5aa7816e5b5e506e
sarpong <abasimisarpong@rocketmail.com>
2019-06-16 12:24:24 +0100

    added code to let express hangle 404 pages

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 
 require('dotenv').config()
 app.use('/api/movies', require('./routes/api/movies'))
+app.use('*', require('./routes/errorpages/404'))
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, ()=>{
