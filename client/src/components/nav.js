adb91c4d2d2353e96eba10c426e33c3af84adea5
sarpong <abasimisarpong@rocketmail.com>
2019-06-24 18:00:36 +0100

    I wrote tests and added Discover Movies and Tv shows routes to nav

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import '../styles/nav.css';
 
 export const Nav = (props)=>{
   return(
    <div className="navbar">
+     <div className='navElementsContainer'>
+       <ul className='movies'>
+         <Link to='/discover' className='links'><li id='discover'>Discover</li> </Link>
+         <Link to='/movies' className='links'><li id='movies'>Movies</li></Link>
+         <Link to='/tvshows' className='links'><li id='tv-shows'>TV SHOWS</li></Link>
+       </ul>
+       <ul className='register'>
+         <li id='login'>LOGIN</li>
+         <li id='sign-up'>SIGN UP</li>
+       </ul>
+     </div>
    </div>
   );
-}
-
-Nav.propTypes = {
-  link: PropTypes.array
 }
