b1c94337641b281a6bf9ee7cd2eca05af23f7c11
sarpong <abasimisarpong@rocketmail.com>
2019-06-29 18:47:50 +0100

    added css sytle to discovery component

--- client/src/components/discover.js
+++ client/src/components/discover.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Movie } from './movie';
+import '../styles/discover.css';
 
 export class Discover extends React.Component{
   constructor(props){
@@ -21,15 +21,11 @@ export class Discover extends React.Component{
 
   render(){
     const { discovermovie } = this.state
-    {console.log(discovermovie )}
     return(<div className='container'>
       {discovermovie.map(movies => {
         return(
-          <div key={movies.id}>
-            { movies.poster_path && 
-            <div> 
-              <img src={`https://image.tmdb.org/t/p/w500${movies.poster_path}`} alt=''/>
-            </div>}
+          <div key={movies.id} className='main-container'>
+            { movies.poster_path && <img src={`https://image.tmdb.org/t/p/w500${movies.poster_path}`} alt=''/> }
           </div>
         );
       })}
